2b1d6c3163dafdc522addfe5057a5f851d8a7f30
Julien Law <julienlaw1@gmail.com>
2013-10-25 21:27:00 -0700

    added inital project files

---
+++ src/mainGUI.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+
+public class mainGUI extends JFrame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
