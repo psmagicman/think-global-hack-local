2e3b371c0d5dd1c4f20dbb4ac60295c995c9da64
Julien Law <julienlaw1@gmail.com>
2013-10-25 21:42:12 -0700

    added code to create a simple GUI frame

--- src/mainGUI.java
+++ src/mainGUI.java
@@ -1,11 +1,57 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 
+import java.awt.*;
+import java.awt.event.*;
+
+import java.util.*;
 
 public class mainGUI extends JFrame {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello");
+	
+	/** Variables **/
+	private JLabel frame_title;
+	private Toolkit toolkit;
+	private Dimension screen;
+	
+	/** End of Variables **/
+	
+	public mainGUI() {
+		
+		// set the frame size
+		setSize(300, 200);
+		toolkit = getToolkit();
+		screen = toolkit.getScreenSize();
+		// set the location of the frame to the middle of the screen
+		setLocation(screen.width / 2 - getWidth() / 2, screen.height / 2 - getHeight() / 2);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
+		defineVariables();
+		setTitle(frame_title.getText());
+		
+		setVisible(true);
+		
 	}
+	
+	/**
+	 * define_variables function
+	 * This function defines the variables for the GUI
+	 */
+	private void defineVariables() {
+		JPanel buttons_area = new JPanel();
+		buttons_area.setBorder(new EmptyBorder(10, 10, 10, 10) );
+		buttons_area.setLayout(new GridLayout(4, 1));
+		
+		// define the buttons and title
+		frame_title = new JLabel("Hackathon!");
 
+		
+		// add the radios to a button group
+
+		
+		// add the buttons to the frame
+		
+		
+	}
+	
+	
 }
