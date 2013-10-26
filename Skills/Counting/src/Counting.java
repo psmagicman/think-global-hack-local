48f739a26bdfd47e760efaaa71a6e47d8f4ceae3
whiteSkar <sypower07@hotmail.com>
2013-10-26 00:53:52 -0700

    Counting - Fix main function location

--- Skills/Counting/src/Counting.java
+++ Skills/Counting/src/Counting.java
@@ -1,10 +1,16 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
 public class Counting 
 {
-
 	public static void main(String[] args) 
 	{
-		
+	    JFrame f = new GameWindow();
+	    GameLogic newGame = new GameLogic();
+	    newGame.SetLevel(1);
+	    Integer randomNum = newGame.GenerateRandomNumber();
+	    JLabel num = new JLabel(randomNum.toString());
+	    num.setVisible(true);
+	    f.add(num);
 	}
-
-
 }
--- Skills/Counting/src/GameWindow.java
+++ Skills/Counting/src/GameWindow.java
@@ -1,24 +1,12 @@
 import javax.swing.*;
 	
-public class GameWindow extends JFrame {
-		
-public GameWindow() {
+public class GameWindow extends JFrame
+{
+	public GameWindow() 
+	{
 		setTitle("My Empty Frame");
 		setSize(300,200); // default size is 0,0
 		setLocation(10,200); // default is 0,0 (top left corner)
 		setVisible(true);
-	  }
-	
-
-
-public static void main(String[] args) {
-	    JFrame f = new GameWindow();
-	    GameLogic newGame = new GameLogic();
-	    newGame.SetLevel(1);
-	    Integer randomNum = newGame.GenerateRandomNumber();
-	    JLabel num = new JLabel(randomNum.toString());
-	    num.setVisible(true);
-	    f.add(num);
-	    
-	  }
+	}
 }
