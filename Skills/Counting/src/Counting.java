68db218a9ea2ee463b7a8caf3d3fd98fca520e33
whiteSkar <sypower07@hotmail.com>
2013-10-25 23:46:02 -0700

    GameLogic class added

--- Skills/Counting/src/Counting.java
+++ Skills/Counting/src/Counting.java
@@ -1,8 +1,10 @@
 
-public class Counting {
+public class Counting 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
---
+++ Skills/Counting/src/GameLogic.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class GameLogic 
+{
+	public static int level;
+	
+	int GenerateRandomNumber()
+	{
+		Random generator = new Random();
+return 1;
+	}
+	
+}
