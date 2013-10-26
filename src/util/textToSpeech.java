9dfc1b5f7b19915e9bd619e1db61543175207da1
PuiYan Denise <denisekwokk@gmail.com>
2013-10-26 13:38:43 -0700

    Added what-to-do-if-error message in textToSpeech for debugging purposes

--- src/util/textToSpeech.java
+++ src/util/textToSpeech.java
@@ -1,7 +1,17 @@
 package util;
-
 import com.sun.speech.freetts.Voice;
 import com.sun.speech.freetts.VoiceManager;
+
+/* TODO: Remove this when done & add jsapi.jar part to installation doc 
+ * IF YOU ARE GETTING ERRORS: 
+ * 	- 	Make sure you added FREETTS.JAR to your build path libraries 
+ *	  	(Right-click project > Properties > Java Build Path > Libraries > Add External JARs 
+ * IF YOU CAN'T HEAR THE AUDIO: 
+ * 	-	Make sure you added JSAPI.JAR to your JRE external library folder
+ * 			(C:\Program Files\Java\jre1.6.*\lib\ext) 
+ * 		or  (C:\Program Files\Java\jdk1.*.*\jre\lib\ext)
+ * If you're still getting errors, find Denise!  	
+*/
 
 public class textToSpeech
 {
