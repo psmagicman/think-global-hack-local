bff12fd99feaea2acc262e9c98fe0251f9121eb9
Douglas Wong <fight320@hotmail.com>
2013-10-26 16:43:24 -0700

    minor tweaks to user interface

--- src/gui/mainGUI.java
+++ src/gui/mainGUI.java
@@ -3,8 +3,7 @@ package gui;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
-import users.User;
-import users.Preferences;
+import users.*;
 import util.DirectoryParser;
 
 import java.awt.*;
@@ -12,44 +11,41 @@ import java.awt.event.*;
 import java.util.*;
 
 public class mainGUI extends JFrame {
-	
+
 	/** Variables **/
 	private JLabel frame_title;
 	private Toolkit toolkit;
 	private Dimension screen;
-	
+
 	/** End of Variables **/
-	
+
 	public mainGUI() {
-		
+
 		// set the frame size
 		setup();
 		toolkit = getToolkit();
-		screen = toolkit.getScreenSize();
-		// set the location of the frame to the middle of the screen
-		setLocation(screen.width / 2 - getWidth() / 2, screen.height / 2 - getHeight() / 2);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new GridLayout());
 		defineVariables();
 		setTitle(frame_title.getText());
-		
+
 		setVisible(true);
-		
+
 	}
-	
+
 	public void setup() {
 		// set the frame size
 		setSize(Toolkit.getDefaultToolkit().getScreenSize());
 		toolkit = getToolkit();
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
+
 		//determines the font for the JButtons
 		Font newButtonFont=new Font("Arial Rounded",Font.BOLD,50);
 		UIManager.put("Button.font", newButtonFont);
 		UIManager.put("Button.foreground", Color.DARK_GRAY);
 		UIManager.put("Button.background", Color.WHITE);
 	}
-	
+
 	/**
 	 * define_variables function
 	 * This function defines the variables for the GUI
@@ -58,14 +54,28 @@ public class mainGUI extends JFrame {
 		JPanel buttons_area = new JPanel();
 		buttons_area.setBorder(new EmptyBorder(10, 10, 10, 10) );
 		buttons_area.setLayout(new GridLayout());
-		
+
 		// define title
 		frame_title = new JLabel("SAM");
 	}
-	
+
 	// create buttons here; this is empty because each menu will override this
 	// to implement buttons for specified screens
 	public void makeButtons() {
 	}
 	
+	public void defaultPref(Preferences name){
+		if (name == null){
+			Font newButtonFont=new Font("Arial Rounded",Font.BOLD,50);
+			UIManager.put("Button.font", newButtonFont);
+			UIManager.put("Button.foreground", Color.DARK_GRAY);
+			UIManager.put("Button.background", Color.WHITE);
+		}
+		else{
+			Font newButtonFont=new Font("Arial Rounded",Font.BOLD,name.getFontSize());
+			UIManager.put("Button.font", newButtonFont);
+			UIManager.put("Button.foreground", name.getFontColour());
+			UIManager.put("Button.background", name.getBackgroundColour());
+		}
+	}
 }
