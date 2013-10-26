0874016915fc963c73ed75a0c8d8cc8716734e00
Douglas Wong <fight320@hotmail.com>
2013-10-26 01:12:26 -0700

    Modified NameGet.java

--- src/NameGet.java
+++ src/NameGet.java
@@ -14,17 +14,29 @@ public class NameGet extends JFrame {
 	/** End of Variables 
 	 * @return **/
 
-	public String NameGet() {
-
+	public NameGet() {
+		
 		/**Retrieve the file name from the directory
 		 * Input: "C:\Hello\AnotherFolder\The File Name.PDF"
 		 * Return: "The File Name.PDF"
 		 */
 
-		String nameGet(String fileName){
-			File f = new File(fileName);
-			return f.getName();
-		}
+		//		String nameGet(String fileName){
+		//			File f = new File(fileName);
+		//			return f.getName();
+		//		}
 
+		// nameToButton();
+	}
+	/**
+	 * @param fileName
+	 * Input: a list of string of file names 
+	 * Return: a list of JButton with the appropriate names
+	 */
+	private void nameToButton(ArrayList<String> fileName){
+		for(int i = 0; i < fileName.size(); i++){
+			JButton fn = new JButton(fileName.get(i));
+			add(fn);
+		}
 	}
 }
