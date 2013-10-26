622d98f4f5abaf61eed3138d7e97cd0bbce34602
tuckerbuchy <tucker.buchy@gmail.com>
2013-10-25 23:02:42 -0700

    added skeleton code

---
+++ src/module/IGame.java
@@ -0,0 +1,7 @@
+package module;
+
+public interface IGame {
+	
+	//this is the basic game starter
+	public Result startGame();
+}
---
+++ src/module/Preferences.java
@@ -0,0 +1,6 @@
+package module;
+
+//This is the user preferences such as color and text color etc.
+public class Preferences {
+
+}
