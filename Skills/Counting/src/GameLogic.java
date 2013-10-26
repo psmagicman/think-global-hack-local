6edb8f20cdb2ab03a8b7287787adf03d9d8af891
whiteSkar <sypower07@hotmail.com>
2013-10-26 01:24:48 -0700

    Counting - Add some more functionalities

--- Skills/Counting/src/GameLogic.java
+++ Skills/Counting/src/GameLogic.java
@@ -5,6 +5,8 @@ public class GameLogic
 	public final int MIN_LEVEL = 1;
 	public final int MAX_LEVEL = 9;
 	
+	private int currentNumber;
+
 	private static int level;	// when creating game, this level will be set according to the user prefernece MAYBE or just Min Level
 	private static final HashMap<Integer, Integer> LevelMultiplier;
 	static
@@ -21,18 +23,44 @@ public class GameLogic
 		LevelMultiplier.put(9,1000000000);
 	}
 	
+	GameLogic(int initialLevel)
+	{
+		if (!SetLevel(initialLevel))
+			level = MIN_LEVEL;
+	}
+	
 	int GenerateRandomNumber()
 	{
 		Random generator = new Random();
-		int randomNumber = generator.nextInt(LevelMultiplier.get(level));
+		int currentNumber = generator.nextInt(LevelMultiplier.get(level));
 		
-		return randomNumber;
+		return currentNumber;
 	}
 	
-	void SetLevel(int newLevel)
+	boolean SetLevel(int newLevel)
 	{
 		if (newLevel >= MIN_LEVEL && newLevel <= MAX_LEVEL)
+		{
 			level = newLevel;
+			return true;
+		}
+		return false;
 	}
 	
+	int GetCurrentNumber()
+	{
+		return currentNumber;
+	}
+	
+	int GetNextNumber()
+	{
+		return currentNumber + 1;
+	}
+	
+	boolean IsThisDigitCorrect(int digit, int position)
+	{
+		String nextNumberInString = new Integer(GetNextNumber()).toString();
+		
+		return nextNumberInString.charAt(position) == digit;	// check char and int conversion
+	}
 }
