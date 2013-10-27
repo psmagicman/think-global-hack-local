0bf0efbe0bde047133f58e9bcb80fe81dfb52b62
Wilbur Yu <yuwilbur@gmail.com>
2013-10-26 20:56:58 -0700

    Finished Game Launcher with Singleton

--- src/TestClassLoaderMain.java
+++ src/TestClassLoaderMain.java
@@ -11,19 +11,23 @@ import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 
+import users.User;
+import users.UserManagementService;
+
 public class TestClassLoaderMain extends JFrame {
 
 	private void createAndShowGUI() {
+		/*
+		 * SIMULATE MAIN MENU
+		 */
 		JButton testButton = new JButton("test");
 		testButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				GameLauncher gameLauncher = new GameLauncher();
-//				gameLauncher.launchGame("Users/triciajose/Documents/think-global-hack-local/Games/game.jar");
-				gameLauncher.launchGame("/Games/Category 1/game.jar");
+				gameLauncher.launchGame(".\\game.jar");
 			}
 		});
-
 		add(testButton);
 	}
 
